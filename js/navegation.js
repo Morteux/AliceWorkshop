72a3d92c69ed20ab1da2d23787d78f33bcdb68bf
Morteux <ignacio.melendezlapi@alum.uca.es>
2023-08-25 12:42:45 +0200

    Update

---
+++ js/navegation.js
@@ -0,0 +1,10 @@
+var tab_archetype_search = "";
+var tab_teams_search = "";
+var tab_user = "";
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    tab_archetype_search = document.getElementById("tab_archetype_search");
+    tab_teams_search = document.getElementById("tab_teams_search");
+    tab_user = document.getElementById("tab_user");
+});
+
