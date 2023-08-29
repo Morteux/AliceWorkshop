c1263c3207e3261a3b3ba6463946a0a2766835c8
Morteux <ignacio.melendezlapi@alum.uca.es>
2023-08-29 15:51:42 +0200

    Scroll to top button. New favicon

--- js/tab_teams_search.js
+++ js/tab_teams_search.js
@@ -6,6 +6,9 @@ var start_index = 0;
 document.addEventListener("DOMContentLoaded", (event) => {
     // Show all teams on page load
     searchQuery();
+    
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () { scrollFunction() };
 
     $(window).scroll(function () {
         if ($(window).scrollTop() == $(document).height() - $(window).height() && document.getElementById("tab_teams_search_button").classList.contains("active_tab_button")) {
@@ -14,6 +17,20 @@ document.addEventListener("DOMContentLoaded", (event) => {
         }
     });
 });
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scroll_top_button").style.display = "block";
+    } else {
+        document.getElementById("scroll_top_button").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 function resetResult() {
     start_index = 0;
