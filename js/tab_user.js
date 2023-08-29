bb3b3b03eada04f2bdb6e26e674473deb4d5a7a7
Morteux <ignacio.melendezlapi@alum.uca.es>
2023-08-29 15:03:36 +0200

    Team creator button copy json to clipboard

--- js/tab_user.js
+++ js/tab_user.js
@@ -85,6 +85,16 @@ function printTeamJSON() {
         }
     }
     `;
+
+    // Get the text field
+    let copyText = document.getElementById("json_result");
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
 }
 
 
