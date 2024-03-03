e112895c7a052758a3f7e49f40358e62c0ab13a9
Morteux <ignis.melendez@gmail.com>
2024-03-03 10:49:45 +0100

    Sync

--- js/tab_characters.js
+++ js/tab_characters.js
@@ -8,7 +8,7 @@ function getMenuCharacterHTML(character_data) {
     }
 
     return `
-    <div id="character_check_` + character_data.name + `" class="character_container" onclick="console.log('` + character_data.name + `')">
+    <div id="character_check_` + character_data.name + `" class="character_container" onclick="printCharacterInfoHTML('` + character_data.name + `')">
         <img class="character_icon ` + (character_data.rarity == "5" ? "character_5_stars" : "character_4_stars") + `" src="https://api.ambr.top/assets/UI/` + character_data.images.nameicon + `.png" alt="Character icon for ` + character_data.name + `">
         ` + (character_data.element != "None" ? `<img class="element_icon" src="images/elements/glow_` + character_data.element.toLowerCase() + `.png">` : "") + `
         <div class="rarity_container">` + STAR_SVG + STAR_SVG + STAR_SVG + STAR_SVG + (character_data.rarity == "5" ? STAR_SVG : "") + `</div>
@@ -25,4 +25,20 @@ function printAllCharacters() {
     }
 
     document.getElementById("menu_characters").innerHTML = menu_characters;
+}
+
+function printCharacterInfoHTML(character_name) {
+    let character_data = characters[character_name];
+    console.log(character_name);
+    document.getElementById("menu_characters").style.display = "none";
+    
+    document.getElementById("menu_characters_info").innerHTML = `
+    <img class="" src="https://api.ambr.top/assets/UI/` + character_data.images.nameicon + `.png">
+    <img class="" src="https://api.ambr.top/assets/UI/` + character_data.images.namegachasplash + `.png">
+    `;
+}
+
+function resetMenuCharacters() {
+    document.getElementById("menu_characters").style.display = "";
+    document.getElementById("menu_characters_info").innerHTML = "";
 }
