a7c95f765b48a2d3fceb87c9cbed587555ec6993
Morteux <ignis.melendez@gmail.com>
2024-03-06 21:33:35 +0100

    Character icon URL fixed

--- js/tab_characters.js
+++ js/tab_characters.js
@@ -50,7 +50,7 @@ function getMenuCharacterHTML(character_data) {
 
     return `
     <div id="character_` + character_data.name + `" class="character_container" onclick="printCharacterInfoHTML('` + character_data.name + `')">
-        <img class="character_icon character_` + character_data.rarity + `_stars" src="../images/characters/` + character_data.images.filename_icon + `.png" alt="Character icon for ` + character_data.name + `">
+        <img class="character_icon character_` + character_data.rarity + `_stars" src="images/characters/` + character_data.images.filename_icon + `.png" alt="Character icon for ` + character_data.name + `">
         ` + (character_data.elementText != "None" ? `<img class="element_icon" src="images/elements/glow_` + character_data.elementText.toLowerCase() + `.png">` : "") + `
         <div class="rarity_container">` + STAR_SVG + STAR_SVG + STAR_SVG + STAR_SVG + (character_data.rarity == "5" ? STAR_SVG : "") + `</div>
         <div class="character_name ` + (character_data.name.length < SHORT_NAME_LENGTH ? "character_name_short" : (character_data.name.length < MEDIUM_NAME_LENGTH ? "character_name_medium" : "character_name_long")) + `">` + character_data.name + `</div>
@@ -316,7 +316,7 @@ function printCharacterInfoHTML(character_name) {
                 </div>
             </div>
 
-            <img class="menu_character_icon menu_character_` + character_data.rarity + `_stars" src="../images/characters/` + character_data.images.filename_icon + `.png" alt="Character icon for ` + character_data.name + `">
+            <img class="menu_character_icon menu_character_` + character_data.rarity + `_stars" src="images/characters/` + character_data.images.filename_icon + `.png" alt="Character icon for ` + character_data.name + `">
 
             <div class="menu_character_row_info menu_character_row_dark">
                 Rarity:
