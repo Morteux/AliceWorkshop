0140a20db5062fe46734a772f7dfa5634a4d4352
PedroJavierCasadoGalvez <65668707+PedroJavierCasadoGalvez@users.noreply.github.com>
2024-03-20 16:51:39 +0100

    Update teams.js

--- json/teams.js
+++ json/teams.js
@@ -14140,7 +14140,7 @@ const teams = {
         "archetype": [
             "Vaporize"
         ],
-        "viability": "Viable",
+        "viability": "Meta",
         "character_1": {
             "name": "Klee",
             "build": "Standard"
