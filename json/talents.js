004c2a05cc5370bcfe2f9600ed455e318ddc54ea
IMLARfuture <nmelendez@arfuture.es>
2024-04-27 14:27:11 +0200

    Talents fixed

--- json/talents.js
+++ json/talents.js
@@ -41,12 +41,12 @@ const talents = {
             "description": ""
         },
         "passive2": {
-            "name": "Passive 1",
+            "name": "Passive 2",
             "descriptionRaw": "",
             "description": ""
         },
         "passive3": {
-            "name": "Passive 1",
+            "name": "Passive 3",
             "descriptionRaw": "",
             "description": ""
         },
@@ -245,9 +245,9 @@ const talents = {
         "id": 0,
         "name": "Sigewinne",
         "combat1": {
-            "name": "Normal Attack: Targeted Treatment Normal Attack: Performs up to 3 consecutive attacks with a bow. Charged Attack Performs a more precise Aimed Shot with increased DMG. While aiming, the power of Hydro will continually accumulate on the arrow. After fully charging, Sigewinne will periodically fire slow-moving Mini-Stration Bubbles toward the target, dealing Hydro DMG. When released, an arrow imbued with torrential energy will deal Hydro DMG to the opponent hit. Plunging Attack Fires off a shower of arrows in mid-air before falling and striking the ground, dealing AoE DMG upon impact.",
-            "descriptionRaw": "",
-            "description": "",
+            "name": "Normal Attack: Targeted Treatment",
+            "descriptionRaw": "Normal Attack: Performs up to 3 consecutive attacks with a bow. Charged Attack Performs a more precise Aimed Shot with increased DMG. While aiming, the power of Hydro will continually accumulate on the arrow. After fully charging, Sigewinne will periodically fire slow-moving Mini-Stration Bubbles toward the target, dealing Hydro DMG. When released, an arrow imbued with torrential energy will deal Hydro DMG to the opponent hit. Plunging Attack Fires off a shower of arrows in mid-air before falling and striking the ground, dealing AoE DMG upon impact.",
+            "description": "Normal Attack: Performs up to 3 consecutive attacks with a bow. Charged Attack Performs a more precise Aimed Shot with increased DMG. While aiming, the power of Hydro will continually accumulate on the arrow. After fully charging, Sigewinne will periodically fire slow-moving Mini-Stration Bubbles toward the target, dealing Hydro DMG. When released, an arrow imbued with torrential energy will deal Hydro DMG to the opponent hit. Plunging Attack Fires off a shower of arrows in mid-air before falling and striking the ground, dealing AoE DMG upon impact.",
             "attributes": {
                 "labels": [
                 ],
@@ -256,9 +256,9 @@ const talents = {
             }
         },
         "combat2": {
-            "name": " E. Rebound Hydrotherapy Elemental Skill: Blows a Bolstering Bubblebalm that can stimulate blood flow and help patients drift off to sleep using a bubblegun specially made in the Fortress of Meropide. Bolstering Bubblebalms will bounce between nearby opponents, dealing Hydro DMG based on Sigewinne's max HP to opponents it hits. When it hits an opponent, it will restore HP to all nearby party members except Sigewinne herself. The amount healed is based on Sigewinne's max HP. After bouncing 5 times, the Bolstering Bubblebalm will disappear and restore HP to Sigewinne herself based on her own max HP value. When no opponents are present, the Bolstering Bubblebalm will bounce nearby. Only one Bolstering Bubblebalm created by a Sigewinne may exist at once. Hold Enter Aiming Mode, and blow an even bigger, even more breathtaking Bolstering Bubblebalm! The Bolstering Bubblebalm will grow bigger the longer the skill is Held, until it grows in size twice. For each tier in size it grows, the DMG it deals to opponents it hits increases by 5%, and the healing provided to nearby characters increases by 5%. After weaker opponents are hit by big Bolstering Bubblebalms, they will be imprisoned and will be unable to move. When big Bolstering Bubblebalms bounce, they will drop down a tier in size, until they return to being normal Bolstering Bubblebalms. In addition, 2 Sourcewater Droplets will be created near Sigewinne when she uses Rebound Hydrotherapy. Each Sourcewater Droplet Sigewinne absorbs will grant her a Bond of Life worth 10% of her Max HP. When Sigewinne's Bond of Life is cleared, she regains 1 Elemental Energy for every 2,000 HP worth of the Bond of Life that was cleared. Sigewinne can regain up to 5 Elemental Energy in this way. Arkhe: Ousia Bolstering Bubblebalms periodically call down a Surging Blade on the position they hit an opponent, dealing Ousia-aligned Hydro DMG based on Sigewinne's max HP. Hold the skill to use it in a different manner.",
-            "descriptionRaw": "",
-            "description": "",
+            "name": "E. Rebound Hydrotherapy",
+            "descriptionRaw": "Elemental Skill: Blows a Bolstering Bubblebalm that can stimulate blood flow and help patients drift off to sleep using a bubblegun specially made in the Fortress of Meropide. Bolstering Bubblebalms will bounce between nearby opponents, dealing Hydro DMG based on Sigewinne's max HP to opponents it hits. When it hits an opponent, it will restore HP to all nearby party members except Sigewinne herself. The amount healed is based on Sigewinne's max HP. After bouncing 5 times, the Bolstering Bubblebalm will disappear and restore HP to Sigewinne herself based on her own max HP value. When no opponents are present, the Bolstering Bubblebalm will bounce nearby. Only one Bolstering Bubblebalm created by a Sigewinne may exist at once. Hold Enter Aiming Mode, and blow an even bigger, even more breathtaking Bolstering Bubblebalm! The Bolstering Bubblebalm will grow bigger the longer the skill is Held, until it grows in size twice. For each tier in size it grows, the DMG it deals to opponents it hits increases by 5%, and the healing provided to nearby characters increases by 5%. After weaker opponents are hit by big Bolstering Bubblebalms, they will be imprisoned and will be unable to move. When big Bolstering Bubblebalms bounce, they will drop down a tier in size, until they return to being normal Bolstering Bubblebalms. In addition, 2 Sourcewater Droplets will be created near Sigewinne when she uses Rebound Hydrotherapy. Each Sourcewater Droplet Sigewinne absorbs will grant her a Bond of Life worth 10% of her Max HP. When Sigewinne's Bond of Life is cleared, she regains 1 Elemental Energy for every 2,000 HP worth of the Bond of Life that was cleared. Sigewinne can regain up to 5 Elemental Energy in this way. Arkhe: Ousia Bolstering Bubblebalms periodically call down a Surging Blade on the position they hit an opponent, dealing Ousia-aligned Hydro DMG based on Sigewinne's max HP. Hold the skill to use it in a different manner.",
+            "description": "Elemental Skill: Blows a Bolstering Bubblebalm that can stimulate blood flow and help patients drift off to sleep using a bubblegun specially made in the Fortress of Meropide. Bolstering Bubblebalms will bounce between nearby opponents, dealing Hydro DMG based on Sigewinne's max HP to opponents it hits. When it hits an opponent, it will restore HP to all nearby party members except Sigewinne herself. The amount healed is based on Sigewinne's max HP. After bouncing 5 times, the Bolstering Bubblebalm will disappear and restore HP to Sigewinne herself based on her own max HP value. When no opponents are present, the Bolstering Bubblebalm will bounce nearby. Only one Bolstering Bubblebalm created by a Sigewinne may exist at once. Hold Enter Aiming Mode, and blow an even bigger, even more breathtaking Bolstering Bubblebalm! The Bolstering Bubblebalm will grow bigger the longer the skill is Held, until it grows in size twice. For each tier in size it grows, the DMG it deals to opponents it hits increases by 5%, and the healing provided to nearby characters increases by 5%. After weaker opponents are hit by big Bolstering Bubblebalms, they will be imprisoned and will be unable to move. When big Bolstering Bubblebalms bounce, they will drop down a tier in size, until they return to being normal Bolstering Bubblebalms. In addition, 2 Sourcewater Droplets will be created near Sigewinne when she uses Rebound Hydrotherapy. Each Sourcewater Droplet Sigewinne absorbs will grant her a Bond of Life worth 10% of her Max HP. When Sigewinne's Bond of Life is cleared, she regains 1 Elemental Energy for every 2,000 HP worth of the Bond of Life that was cleared. Sigewinne can regain up to 5 Elemental Energy in this way. Arkhe: Ousia Bolstering Bubblebalms periodically call down a Surging Blade on the position they hit an opponent, dealing Ousia-aligned Hydro DMG based on Sigewinne's max HP. Hold the skill to use it in a different manner.",
             "attributes": {
                 "labels": [
                 ],
@@ -267,9 +267,9 @@ const talents = {
             }
         },
         "combat3": {
-            "name": "Q. Super Saturated Syringing Burst Skill: Take out a special Fortress of Meropide-made syringe and assault the area in front with waves of kindness and medicine, dealing AoE Hydro DMG. In addition, Sigewinne absorb up to 2 nearby Sourcewater Droplets within a certain range right before using the skill.",
-            "descriptionRaw": "",
-            "description": "",
+            "name": "Q. Super Saturated Syringing",
+            "descriptionRaw": "Burst Skill: Take out a special Fortress of Meropide-made syringe and assault the area in front with waves of kindness and medicine, dealing AoE Hydro DMG. In addition, Sigewinne absorb up to 2 nearby Sourcewater Droplets within a certain range right before using the skill.",
+            "description": "Burst Skill: Take out a special Fortress of Meropide-made syringe and assault the area in front with waves of kindness and medicine, dealing AoE Hydro DMG. In addition, Sigewinne absorb up to 2 nearby Sourcewater Droplets within a certain range right before using the skill.",
             "attributes": {
                 "labels": [
                 ],
@@ -278,17 +278,17 @@ const talents = {
             }
         },
         "passive1": {
-            "name": "Requires Appropriate Rest Sigewinne grants herself the 'Semi-Sict Bedrest' effect for 18s after using Rebound Hydrotherapy: Sigewinne gains an 8% Hydro DMG Bonus and 10 stacks of Convalescence. When the Elemental Skills of your own characters on standby other than Sigewinne deal DMG, consume 1 stack of Convalescence, and increase the DMG dealt by this instance of Elemental Skill DMG. Every 1,000 HP Sigewinne has above 30,000 increases the DMG by 65. The maximum DMG increase for Elemental Skills that can be gained in this way is 1,800.",
-            "descriptionRaw": "",
-            "description": ""
+            "name": "Requires Appropriate Rest",
+            "descriptionRaw": "Sigewinne grants herself the 'Semi-Sict Bedrest' effect for 18s after using Rebound Hydrotherapy: Sigewinne gains an 8% Hydro DMG Bonus and 10 stacks of Convalescence. When the Elemental Skills of your own characters on standby other than Sigewinne deal DMG, consume 1 stack of Convalescence, and increase the DMG dealt by this instance of Elemental Skill DMG. Every 1,000 HP Sigewinne has above 30,000 increases the DMG by 65. The maximum DMG increase for Elemental Skills that can be gained in this way is 1,800.",
+            "description": "Sigewinne grants herself the 'Semi-Sict Bedrest' effect for 18s after using Rebound Hydrotherapy: Sigewinne gains an 8% Hydro DMG Bonus and 10 stacks of Convalescence. When the Elemental Skills of your own characters on standby other than Sigewinne deal DMG, consume 1 stack of Convalescence, and increase the DMG dealt by this instance of Elemental Skill DMG. Every 1,000 HP Sigewinne has above 30,000 increases the DMG by 65. The maximum DMG increase for Elemental Skills that can be gained in this way is 1,800."
         },
         "passive2": {
-            "name": "Detailed Diagnosis, Thorough Treatment Sigewinne gains a healing bonus based on the total current value of Bonds of Life on all party members. For each 1,000 HP worth of Bonds of Life on party members, Sigewinne gains a 3% Healing Bonus. The maximum Healing Bonus that can be obtained in this way is 30%.",
-            "descriptionRaw": "",
-            "description": ""
+            "name": "Detailed Diagnosis, Thorough Treatment",
+            "descriptionRaw": "Sigewinne gains a healing bonus based on the total current value of Bonds of Life on all party members. For each 1,000 HP worth of Bonds of Life on party members, Sigewinne gains a 3% Healing Bonus. The maximum Healing Bonus that can be obtained in this way is 30%.",
+            "description": "Sigewinne gains a healing bonus based on the total current value of Bonds of Life on all party members. For each 1,000 HP worth of Bonds of Life on party members, Sigewinne gains a 3% Healing Bonus. The maximum Healing Bonus that can be obtained in this way is 30%."
         },
         "passive3": {
-            "name": "Passive 1",
+            "name": "Passive 3",
             "descriptionRaw": "",
             "description": ""
         },
@@ -525,12 +525,12 @@ const talents = {
             "description": ""
         },
         "passive2": {
-            "name": "Passive 1",
+            "name": "Passive 2",
             "descriptionRaw": "",
             "description": ""
         },
         "passive3": {
-            "name": "Passive 1",
+            "name": "Passive 3",
             "descriptionRaw": "",
             "description": ""
         },
