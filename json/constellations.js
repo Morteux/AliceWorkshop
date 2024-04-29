26dd28815a7faab38e9b60c7560785d91dc6e57b
Kryon GG <65668707+KryonGG@users.noreply.github.com>
2024-04-29 15:03:54 +0200

    Update constellations.js

--- json/constellations.js
+++ json/constellations.js
@@ -3,33 +3,33 @@ const constellations = {
         "id": 0,
         "name": "Sethos",
         "c1": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "Sealed Shrine's Spiritsong",
+            "descriptionRaw": "The CRIT Rate of Shadowpiercing Shot is increased by 15%.",
             "description": ""
         },
         "c2": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "2. Papyrus Scripture of Silent Secrets",
+            "descriptionRaw": "When the following conditions are met, Sethos gains a 15% Electro DMG Bonus for 10s (may stack up to 2 times, counted independently): ·Regaining Elemental Energy via Ancient Rite: The Thundering Sands; ·Using Secret Rite: Twilight Shadowpiercer; ·Consuming Elemental Energy through the Passive Talent 'Black Kite's Enigma.' You must first unlock 'Black Kite's Enigma.'",
             "description": ""
         },
         "c3": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "Ode to the Moonrise Sage",
+            "descriptionRaw": "Increases the Level of Normal Attack: Royal Reed Bowmanship by 3. Maximum upgrade level is 15.",
             "description": ""
         },
         "c4": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "Beneficent Plumage",
+            "descriptionRaw": "When Shadowpiercing Shot or Dusk Bolt strike 2 or more opponents, all nearby party members gain 80 Elemental Mastery for 10s.",
             "description": ""
         },
         "c5": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "5. Record of the Desolate God's Burning Sands",
+            "descriptionRaw": "Increases the Level of Secret Rite: Twilight Shadowpiercer by 3. Maximum upgrade level is 15.",
             "description": ""
         },
         "c6": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "Tome of the Sojourning Sun Temple",
+            "descriptionRaw": "After Shadowpiercing Shot strikes an opponent, the Elemental Energy consumed by the Passive Talent 'Black Kite's Enigma' will be returned. This effect can be triggered up to once every 15s. You must first unlock the Passive Talent 'Black Kite's Enigma.'",
             "description": ""
         },
         "images": {
@@ -47,33 +47,33 @@ const constellations = {
         "id": 0,
         "name": "Sigewinne",
         "c1": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "Can the Happiest of Spirits Understand Anxiety?",
+            "descriptionRaw": "The first 3 bounces of Rebound Hydrotherapy's Bolstering Bubblebalm will not cause a larger Bubblebalm to become smaller, and it can bounce 3 times more. Additionally, when a Bubblebalm bounces, it will grant Sigewinne 1 stack to her Convalescence tally. Must unlock the Passive Talent 'Requires Appropriate Rest' first.",
             "description": ""
         },
         "c2": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "Can the Most Merciful of Spirits Defeat Its Foes?",
+            "descriptionRaw": "When using Rebound Hydrotherapy and Super Saturated Syringing, Sigewinne can create a Bubbly Shield worth 30% of her Max HP that absorbs Hydro DMG with 250% efficiency. Additionally, when Rebound Hydrotherapy's Bolstering Bubblebalm or Super Saturated Syringing hits an opponent, that opponent's Hydro RES will be decreased by 35% for 8s.",
             "description": ""
         },
         "c3": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "Can the Healthiest of Spirits Cure Fevers?",
+            "descriptionRaw": "Increases the Level of Rebound Hydrotherapy by 3. Maximum upgrade level is 15.",
             "description": ""
         },
         "c4": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "Can the Loveliest of Spirits Keep Decay at Bay?",
+            "descriptionRaw": "The duration of Super Saturated Syringing is extended by 3s.",
             "description": ""
         },
         "c5": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "Can the Most Joyful of Spirits Alleviate Agony?",
+            "descriptionRaw": "Increases the Level of Super Saturated Syringing by 3. Maximum upgrade level is 15.",
             "description": ""
         },
         "c6": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "Would the Most Radiant of Spirits Pray For Me?",
+            "descriptionRaw": "When Sigewinne performs healing, she will increase the CRIT Rate and CRIT DMG of her Super Saturated Syringing based on her Max HP. Every 1,000 Max HP she has will increase CRIT Rate by 0.7% and CRIT DMG by 1.6% for 15s. The maximum increase achievable this way is 20% CRIT Rate and 110% CRIT DMG.",
             "description": ""
         },
         "images": {
@@ -91,33 +91,33 @@ const constellations = {
         "id": 0,
         "name": "Clorinde",
         "c1": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "From This Day, I Pass the Candle's Shadow-Veil",
+            "descriptionRaw": "While Hunt the Dark's Night Watch state is active, when Electro DMGfrom Clorinde's Normal Attacks hit opponents, they will trigger 2 coordinated attack from a Nightwatch Shade summoned near the hit opponent, each dealing 30% of Clorinde's ATK as Electro DMG. This effect can occur once every 1s. DMG dealt this way is considered Normal Attack DMG.",
             "description": ""
         },
         "c2": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "Now, As We Face the Perils of the Long Night",
+            "descriptionRaw": "The Passive Talent 'Dark-Shattering Flame' will be buffed: After a nearby party member triggers a Electro-related reaction against an opponent, Electro DMG dealt by Normal Attacks and Last Lightfall will be increased by 24% of Clorinde's ATK for 15s. Max 3 buff stacks. Each stack is counted independently. When you have 3 stacks, Clorinde's interruption resistance will be increased. The Maximum DMG increase achievable through such attacks is 2,160. You must first unlock the Passive Talent 'Dark-Shattering Flame.'",
             "description": ""
         },
         "c3": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "I Pledge to Remember the Oath of Daylight",
+            "descriptionRaw": "Increases the Level of Hunt the Dark by 3. Maximum upgrade level is 15.",
             "description": ""
         },
         "c4": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "To Enshrine Tears, Life, and Love",
+            "descriptionRaw": "When Last Lightfall deals DMG to opponent(s), DMG dealt is increased based on Clorinde's Bond of Life percentage. Every 1% of her current Bond of Life will increase Last Lightfall DMG by 2%. The maximum Last Lightfall DMG increase achievable this way is 200%.",
             "description": ""
         },
         "c5": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "Holding Dawn's Coming as My Votive",
+            "descriptionRaw": "Increases the Level of Last Lightfall by 3. Maximum upgrade level is 15.",
             "description": ""
         },
         "c6": {
-            "name": "",
-            "descriptionRaw": "",
+            "name": "And So Shall I Never Despair",
+            "descriptionRaw": "For 12s after Hunt the Dark is used, Clorinde's CRIT Rate will be increased by 10%, and her CRIT DMG by 70%. Additionally, while Night Watch is active, a Glimbright Shade will appear under specific circumstances, decreasing DMG dealt to Clorinde by 80% for 1s and increasing her interruption resistance; it will also attack opponents, dealing 200% of Clorinde's ATK as Electro DMG. DMG dealt this way is considered Normal Attack DMG. The Glimbright Shade will appear under the following circumstances: · When Clorinde is about to be attacked by an opponent. · When Clorinde uses Impale the Night: Pact. 1 Glimbright Shade can be summoned in the aforementioned ways every Xs. 6 Shades can be summoned per single Night Watch duration.",
             "description": ""
         },
         "images": {
